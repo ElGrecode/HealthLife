9eb2408a267093f7727b87dafc15d1490e792321
narwhaldo <narwhaldo@gmail.com>
2014-09-27 08:48:48 -0700

    initial commit establishes project and healthkit inclusion

---
+++ README.md
@@ -0,0 +1,16 @@
+# HealthLife
+An app that uses Apple's Health Kit SDK to track calories
+
+### Authors
+Levani Maisouradze
+Ian Goldfarb
+Adam Schuld
+
+### Version
+'0.0.1'
+
+### Installation on Device
+Must have iOS 8.0.2 or later for installation on device
+
+### Github Hackathon
+An app built during a 9/27 Hackathon in San Francisco
---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HealthLife
+//
+//  Created by Adam Schuld on 9/27/14.
+//  Copyright (c) 2014 Adam Schuld. All rights reserved.
+//
+
+import UIKit
+import HealthKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
